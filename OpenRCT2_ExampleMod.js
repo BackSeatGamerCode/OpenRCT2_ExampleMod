b2ab1b5f9fc13f9090b96d982ef946a3e1147b4f
Christopher <cpsuperstoreinc@gmail.com>
2022-08-08 00:19:42 -0400

    Added 8 brand new rewards

--- OpenRCT2_ExampleMod.js
+++ OpenRCT2_ExampleMod.js
@@ -9,13 +9,172 @@ var REWARDS = {
     "noMoney":                  function(name, reward, command){ temporaryFlagToggle("noMoney", 1, reward); },
     "open":                     function(name, reward, command){ temporaryFlagToggle("open", 1, reward); },
     "renameRandomRide":         function(name, reward, command){ renameRandomRide(name); },
-    "maxBankLoan":              function(name, reward, command){ park.bankLoan = park.maxBankLoan; },
+    "maxBankLoan":              function(name, reward, command){ park.bankLoan += park.maxBankLoan; },
     "halfRating":               function(name, reward, command){ park.rating = Math.floor(park.rating / 2); },
-    "loan1000":                 function(name, reward, command){ park.bankLoan += 10000; },
+    "loan1000":                 function(name, reward, command){ park.bankLoan += 1000; },
     "renamePark":               function(name, reward, command){ park.name = name; },
     "crash":                    function(name, reward, command){ crashRandomCar() },
     "creeper":                  function(name, reward, command){ creeper() },
-    "sploosh":                  function(name, reward, command){ sploosh() }
+    "sploosh":                  function(name, reward, command){ sploosh() },
+    "fireEmployee":             function(name, reward, command){ fireEmployee() },
+    "entertainPatron":          function(name, reward, command){ entertainPatron() },
+    "createTrash":              function(name, reward, command){ createTrash() },
+    "writeName":                function(name, reward, command){ writeName(name) },
+    "gift1000":                 function(name, reward, command){ park.cash += 1000 },
+    "pickPocket":               function(name, reward, command){ pickPocket() },
+    "confuse":                  function(name, reward, command){ confuse() },
+    "fireSploosh":              function(name, reward, command){ fireSploosh() }
+}
+
+BANDIT_CLONE_COUNT = 1;
+TRASH_TYPES = [
+    "vomit", "vomit_alt", "empty_can", "rubbish", "burger_box", "empty_cup", "empty_box", 
+    "empty_bottle", "empty_bowl_red", "empty_drink_carton", "empty_juice_cup", "empty_bowl_blue"
+];
+
+CURSES = ["Dinkleberg", "Perry the Platypus", "Bandit"]
+
+TEXT_Y = 1;
+
+function confuse(){
+    var peep = locateRandomEntity("guest", isValidPeep);
+
+    if(peep != null){
+        console.log(peep.name)
+
+        var x = peep.x;
+        var y = peep.y;
+
+        context.setInterval(function () {
+            
+            peep.destination = {
+                x: x,
+                y: y
+            };
+
+        }, 2000);
+    }
+}
+
+function pickPocket(){
+    var peep = locateRandomEntity("guest", isValidPeep);
+
+    if(peep != null){
+        context.setTimeout(function () {
+
+            map.createEntity("money_effect", {
+                x: peep.x,
+                y: peep.y,
+                z: peep.z + 10
+            });
+
+            peep.name = "CURSE YOU " + CURSES[Math.floor(Math.random() * CURSES.length)].toUpperCase();
+            
+            park.cash += peep.cash;
+            peep.cash = 0;
+
+            ui.showError("Pick Pocket Complete", peep.name);
+
+            
+        }, 1000);
+    }
+}
+
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function writeName(name){
+    name = name.toLowerCase();
+
+    var xPos = 40;
+    var yPos = TEXT_Y;
+
+    TEXT_Y += 64;
+
+    for (var i = 0; i < name.length; i++) {
+        var letter = name.charAt(i);
+
+        if(TILE_LETTERS.hasOwnProperty(letter)){
+            var matrix = TILE_LETTERS[letter].reverse();
+
+            for (var y = 0; y < matrix.length; y++) {
+                for (var x = 0; x < matrix[y].length; x++) {
+
+                    if (matrix[y][x] == 1){
+                        map.createEntity("litter", {
+                            x: xPos + (x * 8),
+                            y: yPos + (y * 8),
+                            z: 256
+                        });
+                        // var tile = map.getTile(xPos + x, yPos + y);
+
+                        // floodTile(tile)
+                    }
+                }
+            }
+
+            TILE_LETTERS[letter].reverse()
+        } 
+        xPos += 6 * 8;
+    }
+}
+
+function entertainPatron(){
+    var peep = locateRandomEntity("guest", isValidPeep);
+
+    if(peep != null){
+        context.setTimeout(function () {
+            
+            for (var i = 0; i < 100; i++) {
+                var staff = map.createEntity("staff", {
+                    x: peep.x,
+                    y: peep.y,
+                    z: peep.z
+                });
+                
+                staff.staffType = "entertainer";
+                staff.name = "Bandit Clone " + BANDIT_CLONE_COUNT;
+                staff.orders = 1;
+
+                staff.energy = 100;
+                staff.energyTarget = 100;
+
+                staff.destination = peep.destination;
+
+                BANDIT_CLONE_COUNT ++;
+            }
+
+        }, 1000);
+    }
+}
+
+function createTrash(){
+    var peep = locateRandomEntity("guest", isValidPeep);
+
+    if(peep != null){
+        context.setTimeout(function () {
+            
+            for (var i = 0; i < 100; i++) {
+                var trash = map.createEntity("litter", {
+                    x: peep.x + getRandomInt(-8, 8),
+                    y: peep.y + getRandomInt(-8, 8),
+                    z: peep.z
+                });
+                trash.litterType = TRASH_TYPES[Math.floor(Math.random() * TRASH_TYPES.length)];
+            }
+
+        }, 1000);
+    }
+}
+
+function fireEmployee(){
+    var peep = locateRandomEntity("staff", function(peep) {return peep.staffType != "entertainer";}, false);
+
+    if(peep != null){
+        console.log(peep.name);
+        peep.remove();
+    }
 }
 
 function cartesianProduct(arr1, arr2){
@@ -79,6 +238,27 @@ function getTileLocations(xSize, ySize){
     return result;
 }
 
+
+function fireSploosh(){
+    var x = 1;
+
+    var interval = context.setInterval(function () {
+
+        for (var y = 0; y < map.size.y; y++) {
+            floodTile(map.getTile(x, y));
+        }
+
+        if(x >= map.size.x){
+            context.clearInterval(interval);
+            postMessage("sploosh v2");
+        }
+        
+        x++;
+
+    }, 1000);
+}
+
+
 function sploosh(){
     var deleteInterval = 10;
     var duration = map.size.x * map.size.y * deleteInterval / 1000 / 60;
@@ -111,18 +291,41 @@ function locateRandom(objects){
     return obj;
 }
 
-function locateRandomEntity(type, filter){
+function locateRandomObject(type, filter){
+    if (filter == undefined){
+        filter = function(peep) {return true;}
+    }
+
+    var objects = context.getAllObjects(type);
+    objects = objects.filter(filter)
+
+    if (objects.length > 0){
+        return objects[Math.floor(Math.random() * objects.length)];
+    }
+}
+
+function locateRandomEntity(type, filter, move){
+    if (filter == undefined){
+        filter = function(peep) {return true;}
+    }
+
+    if (move == undefined){
+        move = true;
+    }
+
     var entities = map.getAllEntities(type);
     entities = entities.filter(filter)
 
     if (entities.length > 0){
         var entity = entities[Math.floor(Math.random() * entities.length)];
 
-        var position = ui.mainViewport.getCentrePosition();
-        position.x = entity.x;
-        position.y = entity.y;
+        if (move){
+            var position = ui.mainViewport.getCentrePosition();
+            position.x = entity.x;
+            position.y = entity.y;
 
-        ui.mainViewport.scrollTo(position);
+            ui.mainViewport.scrollTo(position);
+        }
 
         return entity;
     }
@@ -218,10 +421,13 @@ function main() {
     });
     server.listen(29175);
 
+    // context.subscribe('interval.tick', function() {
+    //     park.cash += 1;
+    // });
 }
 
 registerPlugin({
-    name: 'BackSeatGamer OpenRCT2 Example Mod',
+    name: 'BackSeatGamer OpenRCT2 Plugin',
     version: '1.0',
     authors: ['CPSuperstore', "BackSeatGamer"],
     type: 'remote',
@@ -229,3 +435,241 @@ registerPlugin({
     targetApiVersion: 34,
     main: main
 });
+
+
+TILE_LETTERS = {
+    "a": [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+    ],
+    "b": [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+    ],
+    "c": [
+        [0, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 1],
+    ],
+    "d": [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+    ],
+    "e": [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1],
+    ],
+    "f": [
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+    ],
+    "g": [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+    ],
+    "h": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+    ],
+    "i": [
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [1, 1, 1, 1, 1],
+    ],
+    "j": [
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [1, 0, 1, 0, 0],
+        [0, 1, 1, 0, 0],
+    ],
+    "k": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 1, 0],
+        [1, 0, 1, 0, 0],
+        [1, 1, 0, 0, 0],
+        [1, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 0, 0, 0, 1],
+    ],
+    "l": [
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1],
+    ],
+    "m": [
+        [1, 0, 0, 0, 1],
+        [1, 1, 0, 1, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+    ],
+    "n": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 1, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+    ],
+    "o": [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+    ],
+    "p": [
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+    ],
+    "q": [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 0, 1, 0],
+        [0, 1, 1, 0, 1],
+    ],
+    "r": [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 1, 1, 1, 0],
+        [1, 0, 1, 0, 0],
+        [1, 0, 0, 1, 0],
+        [1, 0, 0, 0, 1],
+    ],
+    "s": [
+        [0, 1, 1, 1, 0],
+        [1, 0, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+    ],
+    "t": [
+        [1, 1, 1, 1, 1],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+    ],
+    "u": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 1, 1, 0],
+    ],
+    "v": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+    ],
+    "w": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1],
+        [1, 0, 1, 0, 1],
+        [0, 1, 0, 1, 0],
+    ],
+    "x": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 1, 0],
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+    ],
+    "y": [
+        [1, 0, 0, 0, 1],
+        [1, 0, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+        [0, 0, 1, 0, 0],
+    ],
+    "z": [
+        [1, 1, 1, 1, 1],
+        [0, 0, 0, 0, 1],
+        [0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 0],
+        [0, 1, 0, 0, 0],
+        [1, 0, 0, 0, 0],
+        [1, 1, 1, 1, 1],
+    ],
+}
